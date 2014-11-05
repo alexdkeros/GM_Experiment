a5aca377368c5c7bb447423a958daec9274cd368
ak <ak@ak-PC.lan>
2014-11-05 20:32:36 +0200

    __

---
+++ GM_Exp/GM/MonitoringNode.py
@@ -0,0 +1,78 @@
+'''
+@author: ak
+'''
+from GM_Exp import Config
+from GM_Exp.GM.Node import Node
+
+class MonitoringNode(Node):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, env, inputStream, weight=Config.defWeight, initV=Config.defV,threshold=Config.threshold,monitoringFunction=Config.defMonFunc):
+        '''
+        Constructor
+        '''
+        Node.__init__(self, env, threshold, monitoringFunction)
+        self.inputStream=inputStream
+        self.weight=weight
+        self.v=initV
+        self.vLast=0
+        self.u=0
+        self.delta=0
+        
+        
+    def getWeight(self):
+        return self.weight
+        
+    '''
+    messages methods:
+    incoming: methodName(self,data,sender) format
+    '''
+        
+    def init(self,dat,sender):
+        '''
+            "init" method
+            send signal "init" to Coordinator
+        '''
+        self.vLast=self.v
+        self.send("Coord", "init", (self.vLast,))
+    
+    def req(self,dat, sender):
+        self.rep()
+            
+    def adjSlk(self,dat, sender):
+        dDelta=dat[0]
+        self.delta+=dDelta  #adjusting current slack vector
+        self.u=self.u+(dDelta/self.weight)  #recalculate last drift vector value with new slack vector
+        
+    def newEst(self,dat,sender):
+        self.e=dat[0]
+        self.vLast=self.v
+        self.delta=0
+        
+    def globalViolation(self,dat,sender):
+        pass
+        
+    '''
+    messages methods:
+    outgoing: methodName(self) format
+    '''
+    def rep(self):
+        self.send("Coord", "rep", (self.v,self.u))
+        
+    '''
+    monitoring operation
+    '''
+    def run(self):
+        self.v=self.inputStream.next()
+        self.u=self.e+(self.v-self.vLast)+(self.delta/self.weight)
+        
+        if self.monFunc(self.u)>=self.threshold:
+            self.rep()
+        
+    
+    
+        
+    
