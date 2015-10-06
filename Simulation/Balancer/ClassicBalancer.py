42ab925adc630155ea08e30cd4d251924a4297dc
ak <alexdkeros@gmail.com>
2015-10-06 18:35:48 +0300

    classicBalancer

--- Simulation/Balancer/ClassicBalancer.py
+++ Simulation/Balancer/ClassicBalancer.py
@@ -2,5 +2,17 @@
 @author: ak
 '''
 
-def classicBalancer():
+def classicBalancer(balSet, b, threshold, monFunc, nodeWeightDict):
+    '''
+    Classic balancing function, as in the original Geometric Monitoring Method
+    args:
+        @param balSet: balancing set containing (nodeId, v, u, vel) tuples
+        @param b: balancing vector
+        @param threshold: the monitoring threshold
+        @param monFunc: the monitoring function
+        @param nodeWeightDict: dictionary containing {id: w, }
+    @return {id: dDelta} dictionary
+    '''
+    return {nid:(nodeWeightDict[nid]*b-nodeWeightDict[nid]*u) for (nid,v,u,vel) in balSet}
+    
     
